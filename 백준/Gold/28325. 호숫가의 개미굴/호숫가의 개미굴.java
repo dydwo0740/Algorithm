e11fe9f6551d7a3c5e165592eff584202dfef1f6
이용재 <80679257+dydwo0740@users.noreply.github.com>
2023-12-06 17:47:03 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 444 ms, Memory: 44172 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 import java.util.*;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,42 +15,50 @@ public class Main{
         int[] empty = new int[N]; // 만약 절대 안들어가는 노드면 -1
 
         long ans = 0;
+        int start = -1;
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++){
+
+        for (int i = 0; i < N; i++) {
             long num = Long.parseLong(st.nextToken());
             ans += num;
 
-            if(num != 0){
+            if (num != 0) {
                 empty[i] = -1;
                 visited[i] = true;
+                start = i;
             }
         }
 
-
-        for(int i=0;i<N;i++){
-            if (!visited[i]) {
-                visited[i] = true;
-                int left = i - 1;
-                int right = i + 1;
-                if(i==0){
-                    left = N -1;
-                } else if (i == N - 1) {
+        if(start == -1){
+            start = 0;
+        }
+        for (int i = 0; i < N; i++) {
+            int idx = i + start >= N ? (i + start) % N : i + start;
+            if (!visited[idx]) {
+                visited[idx] = true;
+                int left = idx - 1;
+                int right = idx + 1;
+                if (idx == 0) {
+                    left = N - 1;
+                } else if (idx == N - 1) {
                     right = 0;
                 }
 
                 if (empty[left] == -1 && empty[right] == -1) {
-                    empty[i] = 1;
+                    empty[idx] = 1;
                     ans++;
                 } else if (empty[left] == 1 || empty[right] == 1) {
 
-                } else{
-                    empty[i] = 1;
+                } else {
+                    empty[idx] = 1;
                     ans++;
                 }
+
+                //bw.write(i + " " + ans+" " + empty[left] + " " + empty[right] + "\n");
             }
         }
 
-        bw.write(ans+"\n");
+        bw.write(ans + "\n");
 
         bw.flush();
     }
