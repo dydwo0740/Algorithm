0119dc8b87b8a94dbf830ceb2468bde268d95291
이용재 <80679257+dydwo0740@users.noreply.github.com>
2024-02-10 10:52:14 +0900

    [Gold IV] Title: Square, Not Rectangle, Time: 480 ms, Memory: 46584 KB -BaekjoonHub

--- 백준/Gold/20033. Square， Not Rectangle/Square， Not Rectangle.java
+++ 백준/Gold/20033. Square， Not Rectangle/Square， Not Rectangle.java
@@ -1,41 +1,61 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] v = new int[n];
-        for (int i = 0; i < n; i++)
-            v[i] = sc.nextInt();
 
-        // Check 람다 함수 정의
-        // mid 이상인 수의 연속적인 개수를 세는 함수
-        // mid 이상인 수가 mid개 이상인 경우 true 반환
-        // 아닌 경우 false 반환
-        // Check 함수는 mid 이상인 수의 연속 개수를 세고,
-        // 해당 개수가 mid와 같거나 클 경우 true를 반환하고,
-        // 그렇지 않으면 false를 반환한다.
-        Check check = (mid) -> {
-            int t = 0;
-            for (int i = 0; i < n; i++) {
-                if (v[i] >= mid) t++;
-                else t = 0;
-                if (t >= mid) return true;
-            }
-            return false;
-        };
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int lo = 1, hi = 1000000; // 1부터 1000000까지 범위 설정
-        while (lo + 1 < hi) {
-            int mid = (lo + hi) / 2; // 중간값 계산
-            if (check.check(mid)) lo = mid;
-            else hi = mid;
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] seq = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+            min = Math.min(min, seq[i]);
+            max = Math.max(max, seq[i]);
         }
-        System.out.println(lo);
+
+        int ans = lowerBound(seq, 1, 1000000);
+        bw.write(ans + "\n");
+        bw.flush();
     }
 
-    // Check 함수를 정의하는 인터페이스
-    interface Check {
-        boolean check(int mid);
+    public static int lowerBound(int[] seq, int left, int right) {
+        while (left < right) {
+            int mid = (left + right + 1) / 2; // 이 부분에서 +1을 추가하여 left가 right보다 1 작을 때 바로 종료되도록 함
+            
+            if (isFind(mid, seq)) {
+                left = mid;
+            } else{
+                right = mid - 1; // isFind 결과가 false일 경우, right를 줄임
+            }
+        }
+
+        return left;
+    }
+
+    public static boolean isFind(int target, int[] seq) {
+        int cnt = 0;
+        for (int i = 0; i < seq.length; i++) {
+            if (target <= seq[i]) {
+                cnt++;
+            } else{
+                cnt = 0;
+            }
+
+            if (cnt >= target) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
