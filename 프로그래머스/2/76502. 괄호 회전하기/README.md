f101c4dc650fe573685361b4e23a9f546df9209b
이용재 <80679257+dydwo0740@users.noreply.github.com>
2024-05-29 15:35:34 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.72 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 1.72 ms
+메모리: 76.6 MB, 시간: 1.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 16:38:22
+2024년 05월 29일 15:35:30
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,55 +1,42 @@
-import java.io.*;
 import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        
-        int N = s.length() - 1;
-        
-        if(N == 1){
-            return 0;
-        }
-        
-        for(int i=0;i<N;i++){
-            if(check(s)){
+        for(int i=0;i<s.length();i++){
+            String comp = s.substring(i, s.length()) + s.substring(0, i);
+            if(isGood(comp)){
+                //System.out.println(comp);
                 answer++;
             }
-            s = s.substring(1, s.length()) + s.substring(0,1);
         }
         return answer;
-        
     }
     
-    public boolean check(String s){
+    public boolean isGood(String s){
         Stack<Character> stack = new Stack<>();
         
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i) =='('){
-                stack.push('(');
-            }else if(s.charAt(i) == '{'){
-                stack.push('{');
-            }else if(s.charAt(i) == '['){
-                stack.push('[');
-            }else if(s.charAt(i) == ')'){
-                if(stack.isEmpty() || stack.pop() != '('){
-                    return false;
-                }
-            }else if(s.charAt(i) == '}'){
-                if(stack.isEmpty() || stack.pop() != '{'){
-                    return false;
-                }
-            }else if(s.charAt(i) == ']'){
-                if(stack.isEmpty() || stack.pop() != '['){
-                    return false;
+            if(stack.isEmpty()){
+                stack.push(s.charAt(i));
+            } else{
+                if(s.charAt(i) == ']' || s.charAt(i) == '}' || s.charAt(i) == ')'){
+                    if(s.charAt(i) == ']' && stack.peek() == '['){
+                        stack.pop();
+                    } else if(s.charAt(i) == '}' && stack.peek() == '{'){
+                        stack.pop();
+                    } else if(s.charAt(i) == ')' && stack.peek() == '('){
+                        stack.pop();
+                    }
+                } else{
+                    stack.push(s.charAt(i));
                 }
             }
         }
         
-        if(!stack.isEmpty()){
-                System.out.println(stack.size());
-                return false;
-            }
+        if(stack.isEmpty()){
+            return true;
+        }
         
-        return true;
+        return false;
     }
 }
